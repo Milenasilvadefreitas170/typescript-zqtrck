e7c18fc61de4248511915a7b6162122e9e2dbe4f
Milenasilvadefreitas170 <milenasilvadefreitas170@gmail.com>
2021-11-10 22:09:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-zqtrck
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-zqtrck)
---
+++ index.ts
@@ -0,0 +1,14 @@
+/* Questão 3 */
+enum curso {
+
+  Redes = 'Redes',
+
+  Mecatrônica = 'Mecatrônica',
+
+  Informática = 'Informática',
+
+}
+
+let rede = curso.Redes;
+
+document.write(rede); 
